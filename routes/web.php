e3e08b316267296ec01280b27741ddfca6007707
tomyspt <saputratomy3252gmail.com>
2017-11-02 17:05:12 +0700

    update laravel

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,10 @@ return view('coba1');
 
 Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
 
+Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+
+Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+
 Route::get('coba2', function () {
 return view('coba2');
 });
